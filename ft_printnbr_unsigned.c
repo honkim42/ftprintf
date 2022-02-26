44969f2c8df31977b3d8daba35ac071abc03d0d7
honkim42 <91933777+honkim42@users.noreply.github.com>
2022-02-26 10:33:53 +0900

    Add files via upload

---
+++ ft_printnbr_unsigned.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printnbr.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: honkim <honkim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 10:54:52 by honkim            #+#    #+#             */
+/*   Updated: 2022/02/24 20:54:22 by honkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printnbr_unsigned(unsigned int n)
+{
+	int	len;
+	char	*str;
+
+	len = 0;
+	if (n == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+	else
+	{
+		str = ft_itoa_unsigned(n);
+		len += ft_printstr(str);
+		free(str);
+	}
+	return (len);
+}
